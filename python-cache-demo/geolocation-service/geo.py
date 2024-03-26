63848d7e84d4513445cfac98028e006d17729187
MA3CIN <marcin.zet@onet.pl>
2024-03-26 18:19:34 +0100

    fix cache example

--- python-cache-demo/geolocation-service/geo.py
+++ python-cache-demo/geolocation-service/geo.py
@@ -5,6 +5,7 @@ from requests_cache import CachedSession
 from flask import Flask
 
 infra_svc_endpoint_address = 'http://127.0.0.1:5000/'
+# invalidate cache after 1 hour
 session = CachedSession('infra_owners_cache', backend='sqlite', expire_after=3600)
 
 
@@ -12,25 +13,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_location_for_gate():
-    print("Getting owners:")
     returner_owners = get_owners()
-    print(returner_owners)
-    return(returner_owners)
+    return(str(returner_owners.json()))
 
 # get endpoint IF cache is empty. 
 #invalidate cache if you have a serial number without a corresponding cached owner 
-# invalidate cache after 1 hour
 #with cache enabled
 def get_owners():
-   if (session.cache.contains(infra_svc_endpoint_address)):
+   if (infra_svc_endpoint_address in session.cache.urls()):
       print("gotten from cache")
-      print(session.cache.get(infra_svc_endpoint_address))
-      return session.cache.get(infra_svc_endpoint_address)
    else:
-    response = requests.get(infra_svc_endpoint_address)
-    print("gotten manually")
-   return str(response.json())
-
+      print("gotten manually")
+   return session.get(infra_svc_endpoint_address)
 
 
 if __name__ == '__main__':
