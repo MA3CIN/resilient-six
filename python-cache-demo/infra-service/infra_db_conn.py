85ccb4e9e0a4ba131f8658ed991958eb728925f8
ASypula <aleksandra.sypula@gmail.com>
2024-03-31 22:26:07 +0200

    Basic infra-db examples

---
+++ python-cache-demo/infra-service/infra_db_conn.py
@@ -0,0 +1,44 @@
+import mysql.connector
+import logging
+
+class InfraDBConnector:
+    def __init__(self, host, user, pwd, database="devices", logger=logging.Logger(__name__)):
+        logger.info(f"Connecting to the database {database} on the host: {host}.")
+        try:
+            self.db_conn = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=pwd,
+                database=database
+                )
+            self.cursor = self.db_conn.cursor()
+            logger.info(f"Connection to database {database} established successfully.")
+        except Exception as e:
+            logger.error(f"Cannot connect with database {database} on {host}. Error: {e}.")
+            raise
+
+    def add_device(self, owner, model, x_pos, y_pos):
+        if isinstance(model, str):
+            self.cursor.execute("SELECT id FROM models WHERE name=%s", model)
+            model_id = self.cursor.fetchone()
+        else:
+            model_id=model
+        stmt = "INSERT INTO registered_devices (owner, model, x_position, y_position) VALUES (%s, %s, %s, %s)"
+        self.cursor.execute(stmt, (owner, model_id, x_pos, y_pos))
+        self.db_conn.commit()
+
+    def get_users_devices(self, owner):
+        stmt = "SELECT * FROM registered_devices WHERE owner=%s"
+        self.cursor.execute(stmt, (owner, ))
+        return self.cursor.fetchall()
+
+    def get_all_registered_devices(self):
+        stmt = "SELECT * FROM registered_devices"
+        self.cursor.execute(stmt)
+        return self.cursor.fetchall()
+    
+    def get_all_models(self):
+        stmt = "SELECT * FROM models"
+        self.cursor.execute(stmt)
+        return self.cursor.fetchall()
+    
---
+++ python-cache-demo/infra-service/infra_service.py
@@ -0,0 +1,66 @@
+from infra_db_conn import InfraDBConnector
+from flask import Flask, request, jsonify
+import logging
+
+app = Flask(__name__)
+
+logger = logging.Logger(__name__)
+
+db = InfraDBConnector("localhost", "test_user", "123456")
+
+@app.route('/devices', methods=['GET'])
+def get_all_registered_devices():
+    """
+    Get all registered devices.
+    """
+    logger.info("Getting all registered devices.")
+    return db.get_all_registered_devices()
+
+@app.route('/devices/models', methods=['GET'])
+def get_all_models():
+    """
+    Get all available devices' models.
+    """
+    logger.info("Getting all available models.")
+    return db.get_all_models()
+
+@app.route('/devices/owners/<string:owner_id>', methods=['GET'])
+def get_users_devices(owner_id):
+    """
+    Get all devices belonging to provided user.
+    Args:
+      owner_id (int): id of the owner.
+    """
+    logger.info(f"Fetching devices belonging to user with id {owner_id}.")
+    return db.get_users_devices(owner_id)
+
+@app.route('/devices', methods=['POST'])
+def register_device():
+    """
+    Registering a device.
+    Required input:
+      owner (int): id of the owner
+      model (int): id of the model
+      TODO: maybe we could provide an option to add a name of the model?
+      x_pos (float): x position
+      y_pos (float): y position
+    """
+    data = request.get_json()
+    try:
+      owner = int(data["owner"])
+      model = int(data["model"])
+      x_pos = float(data["x_pos"])
+      y_pos = float(data["y_pos"]) 
+    except Exception as e:
+        logger.error("Incorrect data provided for registering a device. Error: {e}.")
+        raise
+    try:
+      db.add_device(owner, model, x_pos, y_pos)
+      logger.info(f"Devide registered successfully.")
+    except Exception as e:
+      logger.error(f"Error when registering device. Error: {e}.")
+      raise
+    return jsonify(success=True)
+
+if __name__ == '__main__':
+    app.run(port=5000)
