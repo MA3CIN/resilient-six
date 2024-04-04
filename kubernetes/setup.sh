4d4690c3eb8e41470f3ed8a02cb1996013e309cf
MA3CIN <marcin.zet@onet.pl>
2024-04-04 15:21:18 +0200

    Fix HPA. Fix metrics-server for kind cluster

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,6 +1,10 @@
 # get K8s metrics server: ALL FILES!
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
+
+kubectl patch -n kube-system deployment metrics-server --type=json \
+  -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
+  
 # https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale-walkthrough/
 
 # https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale/
