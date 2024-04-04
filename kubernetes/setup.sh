e97d25624ef4830c469a3339220422f378caa38f
Marcin Ziółkowski <marcin.zet@onet.pl>
2024-04-04 12:32:04 +0200

    HPA notes

---
+++ kubernetes/setup.sh
@@ -0,0 +1,6 @@
+# get K8s metrics server: ALL FILES!
+kubectl apply -f https://github.com/nonai/k8s-example-files/tree/main/metrics-server
+
+# https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale-walkthrough/
+
+# https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale/
