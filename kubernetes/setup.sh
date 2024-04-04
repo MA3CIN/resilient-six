cdc0a87ea0448af7501ef7bb500bc99efccf5d4a
MA3CIN <marcin.zet@onet.pl>
2024-04-04 15:10:10 +0200

    add limits to deploys, add HPA, add stress test sh

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,5 +1,5 @@
 # get K8s metrics server: ALL FILES!
-kubectl apply -f https://github.com/nonai/k8s-example-files/tree/main/metrics-server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 # https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale-walkthrough/
 
