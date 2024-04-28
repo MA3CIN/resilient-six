ead337c11d01036e9db66b3749510fc0d4563759
Marcin Ziółkowski <marcin.zet@onet.pl>
2024-04-28 16:09:40 +0200

    2 node cluster aks

--- aks/aks_create.sh
+++ aks/aks_create.sh
@@ -1,1 +1,5 @@
-az aks create -n resilientSixCluster -g resilientSixGroup --generate-ssh-keys --attach-acr resilientsixregistry --node-count 1
+az aks create -n resilientSixCluster -g resilientSixGroup --generate-ssh-keys --attach-acr resilientsixregistry --node-count 2
+
+az aks get-credentials --resource-group resilientSixGroup --name resilientSixCluster
+
+# kubectl apply -f ./yaml .
