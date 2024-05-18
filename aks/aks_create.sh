10c83f93763390d46bb41f9ebb52a08ffdb6e443
Marcin Ziółkowski <marcin.zet@onet.pl>
2024-05-18 10:52:51 +0200

    Create AKV, integrate AKV with AKS

--- aks/aks_create.sh
+++ aks/aks_create.sh
@@ -1,5 +1,8 @@
-az aks create -n resilientSixCluster -g resilientSixGroup --generate-ssh-keys --attach-acr resilientsixregistry --node-count 2
+az aks create -n resilientSixCluster -g resilientSixGroup --generate-ssh-keys --attach-acr resilientsixregistry --node-count 2 --enable-managed-identity --enable-addons azure-keyvault-secrets-provider
 
 az aks get-credentials --resource-group resilientSixGroup --name resilientSixCluster
 
 # kubectl apply -f ./yaml .
+
+
+# kubectl get pods -n kube-system -l 'app in (secrets-store-csi-driver,secrets-store-provider-azure)'
