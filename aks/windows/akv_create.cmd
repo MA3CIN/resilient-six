5b671dc78ee6a7f8c4d41c3ff19b222539a7d581
Marcin Ziółkowski <marcin.zet@onet.pl>
2024-05-26 10:45:06 +0200

    setup AKS <-> AKV, set 3 final ENV for services

--- aks/windows/akv_create.cmd
+++ aks/windows/akv_create.cmd
@@ -1,3 +1,7 @@
 az keyvault create --name resilientsixkeyvault2 --resource-group resilientSixGroup --location polandcentral
 
-az keyvault secret set --vault-name resilientsixkeyvault2 --name ExampleSecret --value MyAKSExampleSecret
+az keyvault secret set --vault-name resilientsixkeyvault2 --name DB_URL --value resilientsix-mysql-db.mysql.database
+
+az keyvault secret set --vault-name resilientsixkeyvault2 --name DB_USER --value admin_user
+
+az keyvault secret set --vault-name resilientsixkeyvault2 --name DB_PSWD --value admin_password
