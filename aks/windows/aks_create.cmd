64e70b7c64d96e3f39169da5b11b3ca653e78679
Marcin Ziółkowski <marcin.zet@onet.pl>
2024-06-09 00:21:15 +0200

    OSM service Mesh

--- aks/windows/aks_create.cmd
+++ aks/windows/aks_create.cmd
@@ -1,3 +1,3 @@
-az aks create -n resilientSixCluster -g resilientSixGroup --generate-ssh-keys --attach-acr resilientsixregistry --node-count 2 --enable-managed-identity --enable-addons azure-keyvault-secrets-provider
+az aks create -n resilientSixCluster -g resilientSixGroup --generate-ssh-keys --attach-acr resilientsixregistry --node-count 2 --enable-managed-identity --enable-addons azure-keyvault-secrets-provider --enable-addons open-service-mesh
 
 az aks get-credentials --resource-group resilientSixGroup --name resilientSixCluster
